b1f8c36a06eca8c1891719c60d01515a3522cc98
Jin Yoo <poiujkl5398@naver.com>
2021-12-06 00:51:13 +0900

    added LvTransferVolume sourse & Header

---
+++ cpp_Folder/LvTransferVolume.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LvTransferVolume.h"
+#include "OpenLvPracCharacter.h"
+#include <Engine/Classes/Components/BoxComponent.h>
+#include <Kismet/GameplayStatics.h>
+
+// Sets default values
+ALvTransferVolume::ALvTransferVolume()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TransferVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("TransferVolum")); 
+	RootComponent = TransferVolume;
+	TransferVolume->SetCollisionProfileName(TEXT("OverlapOnlyPawn"));
+}
+
+// Called when the game starts or when spawned
+void ALvTransferVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALvTransferVolume::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+void ALvTransferVolume::NotifyActorBeginOverlap(AActor* OtherActor) {
+	AOpenLvPracCharacter* Character = Cast<AOpenLvPracCharacter>(OtherActor);
+	if (Character)
+	{
+		UGameplayStatics::OpenLevel(GetWorld(), FName(TransferLevelName));
+	}
+}
